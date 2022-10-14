6ee2628a50a23fa88e0f7e4aa483a7cc1fde21e1
malall <ravi958187@gmail.com>
2022-10-14 10:59:26 +0000

    all

--- src/main/java/training/App.java
+++ src/main/java/training/App.java
@@ -4,10 +4,8 @@ package training;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello"); 
     }
 }
